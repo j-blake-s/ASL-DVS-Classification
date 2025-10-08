62ab6cc7eef7a5c984f37b0f58072ad7ced9f0db
j-blake-s <seekingj@email.sc.edu>
2025-10-08 12:55:16 -0400

    added main file

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from data import get_all, load_data
+from args import parse_args
+
+
+args = parse_args()
+print("="*50)
+print(f"[Dataset]\t{args.dataset.upper()}")
+print(f"[Epochs]\t{args.epochs}")
+print("="*50)
+
+
+
+blake, james, peyton = get_all(args.data_path, args.dataset)
+train_data, test_data = load_data(train=blake[:5] + james[:5], test=peyton)
